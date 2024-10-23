70b92b2d4c69673240feedc95fe9eaf515641b8e
darkblueeggplant <darkblueeggplant@example.com>
2024-10-23 20:18:01 +0300

    read from file

--- 07_example_course/main.go
+++ 07_example_course/main.go
@@ -7,6 +7,10 @@ func main() {
 	//cards.toString()
 	// fmt.Println(cards.toString())
 	cards.saveToFile("my_cards")
+
+	new_cards := newDeckFromFile("my")
+	new_cards.print()
+
 }
 
 // go run main.go deck.go
